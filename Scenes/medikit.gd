2478dd6482e34f34e99ecadf405c55102a14f9ed
pixelcrust <dominik.janser@mail.com>
2023-12-02 12:31:04 +0100

    deleted unused signal

--- Scenes/medikit.gd
+++ Scenes/medikit.gd
@@ -3,7 +3,7 @@ extends Node3D
 @onready var heal_amount = 100
 @onready var interactiontime = 5
 @onready var player = 0
-signal signal_heal()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_tree().get_nodes_in_group("player")[0]
