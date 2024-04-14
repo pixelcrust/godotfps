23274e34d74cc2bec1cbfdf640915f3205df4a87
pixelcrust <dominik.janser@mail.com>
2024-04-14 12:32:21 +0200

    added glass window

---
+++ Scenes/glass.gd
@@ -0,0 +1,19 @@
+extends Node3D
+var hp_start = 1
+var hp = hp_start
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(hp <= 0):
+		die()
+
+func _on_physical_bone_3d_bodypart_hit(dmg, time_rooted):
+	hp -= dmg
+
+func die():
+	queue_free()
