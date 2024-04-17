de4c8c4ca3e3bd00ac429ca418e30d46411174e3
pixelcrust <dominik.janser@mail.com>
2024-04-17 18:04:26 +0200

    worked on the bullet hole code

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-const SPEED = 160.0
+const SPEED = 60.0
 const ACCURACY = 5
 const dmg = 50
 const time_rooted = .5
@@ -47,9 +47,16 @@ func _physics_process(delta):
 				particles.emitting = true
 				
 				#create bullet hole
+				var collision_normal = raycast.get_collision_normal()
 				var new_bullet_hole = bullet_hole.instantiate()
-				new_bullet_hole.position = raycast.get_collision_point()
+				new_bullet_hole.global_transform.origin = raycast.get_collision_point()
 				raycast.get_collider().add_child(new_bullet_hole)
+				#get_tree().root.get_children()[0].add_child(new_bullet_hole)
+				if collision_normal == Vector3.DOWN:
+					new_bullet_hole.rotation_degrees.x = 90
+				elif collision_normal != Vector3.UP:
+					new_bullet_hole.look_at(raycast.get_collision_point() - collision_normal, Vector3(0,1,0))
+					
 				
 			if(raycast.get_collider().is_in_group("has_hp")):
 				print("hit has_hp")
@@ -57,7 +64,7 @@ func _physics_process(delta):
 		raycast.enabled = false
 		await get_tree().create_timer(2.0).timeout
 		queue_free()
-	visibility_cooldown -= 1
+	visibility_cooldown -= .2
 	if visibility_cooldown <= 0:
 		mesh.visible = true
 
