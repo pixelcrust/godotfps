0e37eca18eaeb16b85662c5695f2c0a178a01163
pixelcrust <dominik.janser@mail.com>
2024-05-15 17:11:13 +0200

    gun bullet raycast checks between

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -48,41 +48,13 @@ func _process(delta):
 		abweichung_x = standard_abweichung_x
 		abweichung_y = standard_abweichung_y
 	#print_debug("abweichung_x,abweichung_y:" +str(abweichung_x)+"/"+str(abweichung_y))
-	if Input.is_action_just_pressed("debug_bullet_move"):
-		position += transform.basis * Vector3(SPEED,0+abweichung_y,0+abweichung_x)*delta
+	#if Input.is_action_just_pressed("debug_bullet_move"):
+	position_bullet_before = position
+	position += transform.basis * Vector3(SPEED,0+abweichung_y,0+abweichung_x)*delta
 	visibility_cooldown -= delta
 	
-	
-	
-	
-	"""var MIN_RAYCAST_DISTANCE = 0.05
-	if SPEED > MIN_RAYCAST_DISTANCE:
-		raycast.target_position.z = -SPEED
-		raycast.transform.origin.z = SPEED
-	else:
-		raycast.target_position.z = -MIN_RAYCAST_DISTANCE
-		raycast.transform.origin.z = MIN_RAYCAST_DISTANCE
-	
-		
-		
-		"""
-	#raycast.transform.origin = position_bullet_before
-	#raycast.target_position = global_position
-	#raycast.set_position(position_bullet_before)
-	"""var from = position_bullet_before + Vector3(-10,0,0)
-	var to = global_position
-	var space = get_world_3d().direct_space_state
-	var query = PhysicsRayQueryParameters3D.create(origin, end)
-	query.collide_with_areas = true
-	#raycast.scale.y = 10
-	var ray_query = PhysicsRayQueryParameters3D.new()
-	ray_query.from = from
-	ray_query.to = to
-	ray_query.collide_with_areas = true
-	var raycast_result = space.intersect_ray(ray_query)
-	print(str(raycast_result))"""
 	raycast.global_position = position_bullet_before
-	#raycast.target_position += Vector3(1,1,1)#mesh.global_position
+	raycast.target_position.y = -3
 	
 	
 	if raycast.is_colliding():
@@ -93,8 +65,6 @@ func _process(delta):
 	
 	#execute when target found
 	if(victim != null):
-		
-		
 		print("raycast collision with:" + str(victim) )
 		if((player_shot == true) && (victim.is_in_group("group_player") == true)):
 			pass
@@ -102,8 +72,8 @@ func _process(delta):
 			#mesh.visible = false
 			SPEED = 0
 			global_position = raycast.get_collision_point()
-			if(raycast.get_collider()!= null):
-				if raycast.get_collider().is_in_group("has_blood"):
+			if(victim!= null):
+				if victim.is_in_group("has_blood"):
 					blood_splatter.on = true
 				else:
 					particles.emitting = true
@@ -114,9 +84,9 @@ func _process(delta):
 					new_bullet_hole.global_transform.origin = raycast.get_collision_point()# - Vector3(0,0,0)
 					get_tree().root.get_children()[0].add_child(new_bullet_hole)
 					new_bullet_hole.look_at(raycast.get_collision_point() - collision_normal, Vector3(0,1,0))
-				if(raycast.get_collider().is_in_group("has_hp")):
+				if(victim.is_in_group("has_hp")):
 					#print("hit has_hp")
-					raycast.get_collider().hit(dmg,time_rooted)
+					victim.hit(dmg,time_rooted)
 			visibility_cooldown = 2
 			raycast.set_enabled(false)
 			victim = null
@@ -125,7 +95,7 @@ func _process(delta):
 	elif visibility_cooldown <= 0:
 		mesh.visible = true
 	#print("pos before: "+str(position_bullet_before)+"pos_now: "+str(global_position))
-	position_bullet_before = global_position
+	
 
 func _on_timer_timeout():
 	queue_free()
