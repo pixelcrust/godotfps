e2804a0def4ed9b67137c9efca0a1d465b018920
pixelcrust <dominik.janser@mail.com>
2024-05-19 13:57:49 +0200

    write inventory and position into txt

--- savepoint.gd
+++ savepoint.gd
@@ -1,15 +1,16 @@
 extends StaticBody3D
 
 @onready var outline_mesh = $MeshInstance3D2
-@onready var interactiontime = 3
+@onready var interactiontime = .1
 @onready var player = 0
 @onready var save_data = []
-
+var file = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_tree().get_nodes_in_group("player")[0]
 	save_data.append(player.inventory)
 	save_data.append(player.hp)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,4 +24,6 @@ func use():
 	save_data.append(player.inventory)
 	save_data.append(player.hp)
 	save_data.append(position + Vector3(0,0,-2))
+	file = FileAccess.open("C:/Users/save-/OneDrive/Dokumente/GameDev/fps-save.txt",FileAccess.WRITE)
+	file.store_string(str(save_data))
 	print(save_data)
