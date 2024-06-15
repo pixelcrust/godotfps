0a2ba5c3a2be42c5f9c6f1d418963a5b2fc892d0
Domi <Dominik.janser@mail.com>
2024-06-15 22:56:01 +0200

    destroy glass with sound

--- Scenes/glass.gd
+++ Scenes/glass.gd
@@ -1,6 +1,12 @@
 extends Node3D
 var hp_start = 1
 var hp = hp_start
+@onready var audio_stream_player_3d = $AudioStreamPlayer3D
+@onready var sound_glass_break = preload("res://Sounds/Glass_Break_BeerBottle_702T_Fienup_002.wav")
+var once = 1
+@onready var collision_shape_3d = $CollisionShape3D
+@onready var mesh_instance_3d = $MeshInstance3D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,4 +22,12 @@ func _on_physical_bone_3d_bodypart_hit(dmg, time_rooted):
 	hp -= dmg
 
 func die():
+	if once == 1:
+		once = 0
+		print("hoi")
+		audio_stream_player_3d.stream = sound_glass_break
+		audio_stream_player_3d.play(0.0)
+		collision_shape_3d.disabled = true
+		mesh_instance_3d.visible = false
+	await get_tree().create_timer(2).timeout
 	queue_free()
