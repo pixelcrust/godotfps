3d769b63ffd2ab34966cda6a0d2d09648c7d1604
Domi <Dominik.janser@mail.com>
2024-07-19 12:11:39 +0200

    added ammo pick up

---
+++ ammo_pistol.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@onready var outline_mesh = $MeshInstance3D/MeshInstance3D
+@onready var interaction_time = .01
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func use():
+	#check if gun is in inventory
+	
+	#else dont pick up
+	
+	pass
+	
