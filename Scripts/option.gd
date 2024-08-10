d26ba79431abffd650a516c1c03575b98c9b9c1c
Domi <Dominik.janser@mail.com>
2024-08-10 15:12:58 +0200

    added options for fullscreen

--- Scripts/option.gd
+++ Scripts/option.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var sensitivity_slider = $HSlider
 @onready var audio_stream_player_2d = $AudioStreamPlayer2D
 @onready var SOUND_CLICK = preload("res://Sounds/button_on_off_064.wav")
+@onready var button_fullscreen = $button_fullscreen
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,9 +13,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-
+	
+	if button_fullscreen.button_pressed == false:
+		#minimized
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		#maximized
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		
 func _on_button_back_pressed():
 	audio_stream_player_2d.stream = SOUND_CLICK
 	audio_stream_player_2d.play(0.6)
