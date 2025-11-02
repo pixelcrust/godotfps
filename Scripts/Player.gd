3c5a11fbcecd336c07cfb05b973e6adf638141a6
Domi <Dominik.janser@mail.com>
2025-11-03 00:03:48 +0100

    deleted missing reference

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -23,8 +23,6 @@ var gravity = 9.8
 @onready var head = $Head
 @onready var bone_head = $Head/bone_head
 @onready var bone_body = $MeshInstance3D/bone_body
-@onready var shader_grain: Node3D = $Head/Camera3D/ShaderGrain
-
 
 @onready var camera = $Head/Camera3D
 @onready var guncamera = $Head/Camera3D/CanvasLayer/SubViewportContainer/SubViewport/Camera3D
