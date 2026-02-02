16a9282c3b36314e9fad9e48402151d461ecb906
Domi <Dominik.janser@mail.com>
2026-02-02 09:32:44 +0100

    added pause music

--- music_player.gd
+++ music_player.gd
@@ -4,3 +4,16 @@ var current_track : int = 0
 var current_time_stamp : float = 0
 var paused : bool = false
 var volume : int = 50
+
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
+
+func _process(delta: float) -> void:
+	#if(Input.is_action_just_pressed("key_music_next")):
+		#audio_stream_player.
+		
+	if(Input.is_action_just_pressed("key_music_pause")):
+		if(audio_stream_player.stream_paused == true):
+			audio_stream_player.stream_paused = false
+		else:
+			audio_stream_player.stream_paused = true
