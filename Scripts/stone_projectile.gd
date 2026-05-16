9ec83ffa41bd2ddd57284acdc6963030235cf931
Domi <Dominik.janser@mail.com>
2026-05-17 01:09:30 +0200

    added test for stone

--- Scripts/stone_projectile.gd
+++ Scripts/stone_projectile.gd
@@ -7,7 +7,6 @@ extends RigidBody3D
 @onready var collision_shape_3d_2: CollisionShape3D = $Area3D/CollisionShape3D2
 @onready var area_3d: Area3D = $Area3D
 
-var victim = 0
 @onready var bullet_hole = preload("res://Scenes/effects/bullet_hole.tscn")
 @export var dmg : int = 5
 @export var time_rooted : float = .1
@@ -26,42 +25,41 @@ func _process(delta):
 		once = 1
 	
 	
-	#raycast.force_raycast_update()
-	if area_3d.is_colliding:
-		victim = area_3d.get_collider()
+func _on_timer_timeout():
+	queue_free()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
 		
-	#execute when target found
-	if(victim != null):
-		#print("raycast collision with:" + str(victim) )
-		if(victim.is_in_group("group_player") == true):
+
+	print("stone collision with:" + str(body) )
+	if(body.is_in_group("player_root") == true):
+		print("stone hit player")
+	elif body.is_ancestor_of(self):
+		print("stone hit stone")
+	else:
+		if body.is_in_group("has_blood"):
 			pass
+			#blood_splatter.on = true
 		else:
-			if victim.is_in_group("has_blood"):
-				pass
-				#blood_splatter.on = true
-			else:
 				#particles.emitting = true
-			
+			pass
 				#create bullet hole
-				var collision_normal = collision_shape_3d_2.get_collision_normal()
-				var new_bullet_hole = bullet_hole.instantiate()
-				new_bullet_hole.global_transform.origin = collision_shape_3d_2.get_collision_point()# - Vector3(0,0,0)
-				get_tree().root.get_children()[0].add_child(new_bullet_hole)
-				if collision_normal == Vector3.DOWN:
-					new_bullet_hole.rotation_degrees.x = 90
-				elif collision_normal == Vector3.UP:
-					new_bullet_hole.rotation_degrees.x = -90
-				else:
-					new_bullet_hole.look_at(collision_shape_3d_2.get_collision_point() - collision_normal, Vector3(0,1,0))
-			if(victim.is_in_group("has_hp")):
-				#print("hit has_hp")
-				victim.hit(dmg,time_rooted)
-			victim = null
+			"""var collision_normal = collision_shape_3d_2.get_collision_normal()
+			var new_bullet_hole = bullet_hole.instantiate()
+			new_bullet_hole.global_transform.origin = collision_shape_3d_2.get_collision_point()# - Vector3(0,0,0)
+			get_tree().root.get_children()[0].add_child(new_bullet_hole)
+			if collision_normal == Vector3.DOWN:
+				new_bullet_hole.rotation_degrees.x = 90
+			elif collision_normal == Vector3.UP:
+				new_bullet_hole.rotation_degrees.x = -90
+			else:
+				new_bullet_hole.look_at(collision_shape_3d_2.get_collision_point(collision_normal, Vector3(0,1,0))) -""" 
+		if(body.is_in_group("has_hp")):
+			print("hit has_hp")
+			body.hit(dmg,time_rooted)
 			#raycast.set_enabled(false)
 			#print("raycast disabled")
 			#visibility_cooldown = 2
-			await get_tree().create_timer(2.0).timeout
-			queue_free()
-
-func _on_timer_timeout():
-	queue_free()
+		await get_tree().create_timer(2.0).timeout
+		queue_free()
